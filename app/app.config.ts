6a84a0478a9fc15271968ce2a921789221347943
tonyyang <tonypythoneer@gmail.com>
2026-04-23 22:51:23 +1000

    feat: add crossing into contact list

--- app/app.config.ts
+++ app/app.config.ts
@@ -5,6 +5,8 @@ export default defineAppConfig({
     { label: 'Instagram', url: 'https://www.instagram.com/jenknowsau/', icon: 'i-simple-icons-instagram', hoverClass: 'hover:text-pink-500' },
     { label: 'LinkedIn', url: 'https://www.linkedin.com/in/jenliuau/', icon: 'i-simple-icons-linkedin', hoverClass: 'hover:text-blue-700' },
     { label: 'YouTube', url: 'https://www.youtube.com/@jenliuau', icon: 'i-simple-icons-youtube', hoverClass: 'hover:text-red-600' },
+    { label: 'WordPress', url: 'https://jenliu.com.au/', icon: 'i-simple-icons-wordpress', hoverClass: 'hover:text-blue-500' },
+    { label: 'Crossing', url: 'https://crossing.cw.com.tw/author/1204', icon: 'i-lucide-newspaper', hoverClass: 'hover:text-orange-500' },
     { label: 'Email', url: 'mailto:jen@jenliu.com.au', icon: 'i-lucide-mail', hoverClass: 'hover:text-blue-600' },
   ],
 })
